ea028f541ea44a872cfc65cbc691f543637f8970
André Barros <1211299@isep.ipp.pt>
2023-10-21 15:06:56 +0100

    feat: added date to each bracket

---
+++ src/utils/dateUtils.ts
@@ -0,0 +1,7 @@
+export const formatDateDDStrMonthYYYY = (date: number): string => {
+  const dateObj = new Date(date);
+  const hour = dateObj.getHours();
+  const minutes = dateObj.getMinutes();
+  const month = dateObj.toLocaleString('default', { month: 'short' });
+  return `${dateObj.getDate()} ${month} ${dateObj.getFullYear()} ${hour ? hour : '00'}:${minutes ? minutes : '00'}`;
+};
