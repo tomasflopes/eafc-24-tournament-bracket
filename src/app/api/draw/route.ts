86edaf930ffd67ee850fd193d0d698fc2085ac36
Tomás Lopes <1211289@isep.ipp.pt>
2023-10-29 16:46:07 +0000

    feat: added get tournament route

--- src/app/api/draw/route.ts
+++ src/app/api/draw/route.ts
@@ -1,128 +1,133 @@
 import prisma from "@/app/lib/prisma";
-import { Match, Player } from '@prisma/client';
-import { NextRequest, NextResponse } from 'next/server';
-import { object, array, string, number, optional } from 'zod';
+import { parseDate } from "@/utils/dateUtils";
+import { Player } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { ZodError, array, number, object, string } from "zod";
 
 export async function GET(req: NextRequest) {
+  const matches = await prisma.match.findMany();
 
-    return NextResponse.json([], { status: 200 });
+  const matchesWithPlayers = await Promise.all(
+    matches.map(async (match) => {
+      if (!match.player1Id || !match.player2Id) return match;
+      const player1 = await prisma.player.findUnique({
+        where: {
+          id: match.player1Id,
+        },
+      });
+      const player2 = await prisma.player.findUnique({
+        where: {
+          id: match.player2Id,
+        },
+      });
+
+      return {
+        ...match,
+        player1,
+        player2,
+      };
+    })
+  );
+
+  return NextResponse.json(matchesWithPlayers, { status: 200 });
 }
 
 export async function POST(req: NextRequest) {
-    // draw the tournament bracket
-    try {
+  // draw the tournament bracket
+  try {
+    // schema for the request body
+    const roundSchema = object({
+      start: string(),
+      end: string(),
+      nPcs: number(),
+      gameTime: number(),
+      breakTime: number(),
+    });
 
-        // schema for the request body
-        const roundSchema = object({
-            start: string(),
-            end: string(),
-            nPcs: number(),
-            gameTime: number(),
-            breakTime: number(),
-            playersLeft: optional(number()),
-        });
+    const requestBodySchema = object({
+      rounds: array(roundSchema),
+    });
 
-        const requestBodySchema = object({
-            rounds: array(roundSchema),
-        });
+    // Validate the request body against the schema
+    const requestBody = await req.json();
+    const body = requestBodySchema.parse(requestBody);
+    // valid body
+    const { rounds } = body;
 
-        // Validate the request body against the schema
-        const requestBody = await req.json();
-        requestBodySchema.parse(requestBody);
+    const matches = [];
 
-        // valid body
-        const { rounds } = requestBody;
+    // get players
+    const players: Player[] = await prisma.player.findMany();
+    // randomize players
+    players.sort(() => Math.random() - 0.5);
 
-        const matches = [];
-
-        // get players
-        const players: Player[] = await prisma.player.findMany();
-        // randomize players
-        players.sort(() => Math.random() - 0.5);
-
-        // checks if the number of rounds is log2(nPayers) so the draw can be done
-        if (Math.log2(players.length) != rounds.length) {
-            return NextResponse.json({ error: "The number of rounds must be log2(nPayers)" }, { status: 400 });
-        }
-
-        /* create matches - the following logic was implemented based on this example:
-        
-        32 players
-    
-        FIRST DAY - always 2 games at the same time
-        32/2 = 16 -> 16 games - round 1
-        16/2 = 8 -> 8 games - round 2
-    
-        SECOND DAY - only one game is played at a time
-        8/2 = 4 -> 4 games - round 3 (quarter-finals)
-        4/2 = 2 -> 2 games - round 4 (semi-finals)
-        2/2 = 1 - 1 game - round 5 (final) */
-
-        // num of players
-        let totPlayers = players.length; // total number of players - this will be used to calc the number of rounds
-        let nPlayer = players.length; // number of players - this will be used iterate through the players array till its end
-        // helpers to calc rounds
-        let round = 1;
-        let half = totPlayers / 2;
-
-        // round r - x games till there are y players left -> y = 'rounds[d].playersLeft' (optional field, if none, then 1)
-        for (let r = 0; r < rounds.length; r++) {
-
-            // start date of each round
-            let date = parseDate(rounds[r].start);
-
-            // shcedule matches till rounds[d].playersLeft, if not given the playersLeft, then schedule till there's only 1 player left
-            const pLeft = (rounds[r].playersLeft || 1);
-            while (half >= pLeft) {
-
-                // there's a match per PC
-                for (let j = 0; j < rounds[r].nPcs; j++) {
-
-                    // *info: since this code will shcedule the matches for the rounds in advance, there will be a point 
-                    // where there will be no more players, cuz they still have to play the previous round           
-                    matches.push({
-                        round: round,
-                        player1Id: players[nPlayer - 1]?.id, // *info
-                        player2Id: players[nPlayer - 2]?.id, // *info
-                        startDate: new Date(date), // create new date object to avoid reference
-                    });
-
-                    nPlayer = nPlayer - 2; // 2 players per match
-                    totPlayers -= 2; // 2 players per match
-                }
-
-                date.setMinutes(date.getMinutes() + rounds[r].gameTime + rounds[r].breakTime); // recalc date = minutes per match + minutes break
-
-                // recalc round - a round is finished when all the players played
-                if (totPlayers == 0) {
-                    round++; // next round
-                    totPlayers = half; // reset totPlayers
-                    half = half / 2; // recalc half
-                }
-            }
-        }
-
-        const createdMatches = await prisma.match.createMany({
-            data: matches,
-        });
-
-        return NextResponse.json(createdMatches, { status: 200 });
-
-    } catch (e) {
-        console.log(e);
-
-        return NextResponse.json({ error: "There was an error" }, { status: 500 });
+    // checks if the number of rounds is log2(nPayers) so the draw can be done
+    if (Math.log2(players.length) !== rounds.length) {
+      return NextResponse.json(
+        { error: "The number of rounds must be log2(nPayers)" },
+        { status: 400 }
+      );
     }
+
+    // num of players
+    let totalPlayers = players.length;
+    let playerId = players.length; // player id will be an index of the players array
+    // helpers to calc rounds
+    let round = 1;
+    let half = totalPlayers / 2;
+
+    for (let r = 0; r < rounds.length; r++) {
+      // start date of each round
+      const date = parseDate(rounds[r].start);
+      const gamesForRound = Math.pow(2, rounds.length - r - 1);
+
+      for (let i = 0; i < gamesForRound; i += rounds[r].nPcs) {
+        // there's a match per PC
+        for (let j = 0; j < rounds[r].nPcs; j++) {
+          // *info: since this code will schedule the matches for the rounds in advance, there will be a point
+          // where there will be no more players, cuz they still have to play the previous round
+          matches.push({
+            round: r + 1,
+            player1Id: players[playerId - 1]?.id, // *info
+            player2Id: players[playerId - 2]?.id, // *info
+            startDate: new Date(date), // create new date object to avoid reference
+          });
+
+          playerId = playerId - 2; // 2 players per match
+          totalPlayers -= 2; // 2 players per match
+        }
+
+        date.setMinutes(
+          date.getMinutes() + rounds[r].gameTime + rounds[r].breakTime
+        ); // recalc date = minutes per match + minutes break
+      }
+    }
+
+    const createdMatches = await prisma.match.createMany({
+      data: matches,
+    });
+
+    return NextResponse.json(createdMatches, { status: 200 });
+  } catch (e) {
+    if (e instanceof ZodError)
+      return NextResponse.json({ error: e.errors }, { status: 400 });
+
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 }
+    );
+  }
 }
 
-// date parser - must receive in the format'dd/mm/yyyy hh:mm' 
-function parseDate(date: string): Date {
-    const [datePart, timePart] = date.split(' ');
-    const [day, month, year] = datePart.split('/').map(Number);
-    const [hours, minutes] = timePart.split(':').map(Number);
+export async function DELETE(req: NextRequest) {
+  try {
+    const deletedMatches = await prisma.match.deleteMany();
 
-    const startDateTimeParsed = new Date(Date.UTC(year, month - 1, day, hours, minutes));
-
-
-    return startDateTimeParsed;
+    return NextResponse.json(deletedMatches, { status: 200 });
+  } catch (e) {
+    return NextResponse.json(
+      { error: "Something went wrong" },
+      { status: 500 }
+    );
+  }
 }
--- src/app/api/players/route.ts
+++ src/app/api/players/route.ts
@@ -1,24 +1,15 @@
 import prisma from "@/app/lib/prisma";
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
-// export async function GET(request: Request) {
-//   // placeholders
-//   const players = [
-//     { id: 1, name: "Player 1" },
-//     { id: 2, name: "Player 2" },
-//     { id: 3, name: "Player 3" },
-//     { id: 4, name: "Player 4" },
-//   ];
-
-//   return new Response(JSON.stringify(players), {
-//     headers: { "content-type": "application/json" },
-//   });
-// }
+export async function GET(request: Request) {
+  const players = await prisma.player.findMany();
+  return NextResponse.json(players, { status: 200 });
+}
 
 export async function POST(req: NextRequest) {
   try {
     const { name } = await req.json();
-    
+
     const player = await prisma.player.create({
       data: {
         name,
@@ -26,11 +17,12 @@ export async function POST(req: NextRequest) {
     });
 
     return NextResponse.json(player, { status: 200 });
-
   } catch (e) {
     console.log(e);
-    
-    return NextResponse.json({ error: "There was an error" }, { status: 500 });
-  }
 
+    return NextResponse.json(
+      { error: "There was an error" },
+      { status: 500 }
+    );
+  }
 }
